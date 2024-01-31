194852292533806aaf74b545cfcf9c0577482846
Pedro H. Alves <leonkene18@gmail.com>
2024-01-30 22:27:24 -0300

    molde criado

--- viaCep/App.js
+++ viaCep/App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View>
+        <Text>Consumo da API ViaCep</Text>
+      </View>
+
+      <View>
+
+        <View style={styles.infoInput}>
+          <Text>Informar CEP:</Text>
+          <TextInput placeholder='Cep...'/>
+        </View>
+        
+        <View style={styles.infoInput}>
+          <Text>Logradouro</Text>
+          <TextInput placeholder='Logradouro...'/>
+        </View>
+        
+        <View style={styles.infoInput}>
+          <Text>Bairro</Text>
+          <TextInput placeholder='Bairro...'/>
+        </View>
+        
+        <View style={styles.infoInput}>
+          <Text>Cidade</Text>
+          <TextInput placeholder='Cidade...'/>
+        </View>
+        
+        <View style={styles.infoInput}>
+          <Text>Estado</Text>
+          <TextInput placeholder='Estado...'/>
+
+          <Text>UF</Text>
+          <TextInput placeholder='UF'/>
+        </View>
+        
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +53,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  infoInput: {
+    height: 350,
+    width: `90%`
+  }
 });
